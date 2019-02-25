dba9587c71a60bd616a1d67f985d850008f2ef23
Bartosz Lewiński <bartosz.lewinski@codilime.com>
2019-02-25 10:25:13 +0100

    Added exercises

--- src/hello.ts
+++ src/hello.ts
@@ -1,1 +1,12 @@
-const x: string = 'Hello world';
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter(person: Person): string {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = { firstName: "Typescript", lastName: "fan" };
+
+console.log(greeter(user));
